d0bacae9a8953e674dc9014a83c9861ed414b20b
Emmanuel Nelson <mnelson@onenorth.com>
2014-01-28 15:55:06 -0600

    Almost Working...handlebars installed
    but not configured

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,25 @@
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    var phrases = ["To clean the bagpipes","To drown the wolf bait","To force a double play","To drop the kids off at the pool"];
+    var firstName = req.query.firstname;
+    var lastName = req.query.lastname;
+    var request = require('request');
+    var cheerio = require('cheerio');
+    var url = 'http://celebnetworth.org/' + firstName + '-' + lastName + '-net-worth-salary';
+    var phrase = phrases[(Math.random() * phrases.length) | 0];
+    var amount = 0;
+    request(url, function(err, resp, body) {
+        if (err)
+            throw err;
+        $ = cheerio.load(body);
+        $('.networthtitle p').each(function(ndx, value){
+            if(ndx == 1)
+                amount = $(value).html().replace(/<.*?>.*?<\/.*?>/, "");
+        });
+        $('#celeb').text(firstName + ' ' + lastName);
+        $('#phrase').text(phrase);
+    });
+    res.render('index', { title: 'Express' });
 };
